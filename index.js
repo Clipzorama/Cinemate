f0345f68e1ef29cf88f0c9578fc75c04f496918f
Chrision Wynaar <122323927+Clipzorama@users.noreply.github.com>
2025-01-24 22:04:28 -0500

    updated

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $('.carousel').carousel();
   });
+
+  function changeBg(bg, title) {
+    const banner = document.querySelector(".banner");
+    const contents = document.querySelector(".content");
+    banner.style.background = `url(../images/movies/${bg})`
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+    
+    contents.forEach(content => {
+      content.classList.remove("active");
+      if (content.classList.contains(title)) {
+      content.classList.add("active");
+      }
+    });
+  }
